2e35581e6285703593748a4c64b5a3fd5cf3d6d1
JxBetter <15705834033@163.com>
2018-02-14 17:59:16 +0800

    ok

--- blog/flask_web.py
+++ blog/flask_web.py
@@ -4,17 +4,22 @@ from blog.app.factory import create_app,db
 from blog.app.db_models import Role,User,Article
 from flask_migrate import Migrate
 from flask_login import current_user
+from flask_script import Manager
 
 
 app=create_app(os.environ.get('CONFIG') or 'default')
 migrate=Migrate(app,db)
+manager=Manager(app)
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db,Role=Role,User=User,Article=Article,current_user=current_user)
 
-@app.cli.command()
+@manager.command
 def deploy():
     from flask_migrate import upgrade
     upgrade()
     Role.insert_roles()
+
+if __name__ =='__main__':
+    app.run()
