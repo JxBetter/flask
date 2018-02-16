dad95b166b54e5bb42665926fcd25ba24bb1c723
jx <33095108+JxBetter@users.noreply.github.com>
2018-02-16 19:32:08 +0800

    Update email_fun.py

--- blog/app/email_fun.py
+++ blog/app/email_fun.py
@@ -12,7 +12,7 @@ def send_async_email(app, message):
 def send_email(to, prefix, template, enable=False, **kwargs):
     if enable:
         app = current_app._get_current_object()
-        message = Message(prefix, sender=app.config['MAIL_SENDER'], recipients=[to])
+        message = Message(prefix, sender='15705834033@163.com', recipients=[to])
         message.body = render_template(template + '.txt', **kwargs)
         message.html = render_template(template + '.html', **kwargs)
         thr = Thread(target=send_async_email, args=[app, message])
